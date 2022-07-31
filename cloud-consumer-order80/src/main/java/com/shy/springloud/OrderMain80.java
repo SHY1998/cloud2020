7adf2d3d5fa51a9c8ab62a9a3c498265e751d2f4
Shy <2322621659@qq.com>
2022-07-31 12:23:39 +0800

    单机Eureka构建

--- cloud-consumer-order80/src/main/java/com/shy/springloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/shy/springloud/OrderMain80.java
@@ -2,12 +2,14 @@ package com.shy.springloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author shy
  * @version 1.0
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderMain80  {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain80.class,args);
