d1c89d9884b1995288977580d12031af16f1412b
Shy <2322621659@qq.com>
2022-07-31 10:57:21 +0800

    项目重构，将80和8001共有的entity转移到一个新的共有的模块中

--- cloud-consumer-order80/src/main/java/com/shy/springloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/shy/springloud/controller/OrderController.java
@@ -1,7 +1,7 @@
 package com.shy.springloud.controller;
 
-import com.shy.springloud.entites.CommonResult;
-import com.shy.springloud.entites.Payment;
+import com.shy.springcloud.entites.CommonResult;
+import com.shy.springcloud.entites.Payment;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
