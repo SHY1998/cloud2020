430d564e56f0d4b2725d499f68972ed1a1a7bb4a
Shy <2322621659@qq.com>
2022-08-02 11:49:43 +0800

    更改ribbon的调度算法为随机调用

--- cloud-consumer-order80/src/main/java/com/shy/springloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/shy/springloud/OrderMain80.java
@@ -1,8 +1,11 @@
 package com.shy.springloud;
 
+import com.shy.myRule.MySelfRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClientName;
 
 /**
  * @author shy
@@ -10,6 +13,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @SpringBootApplication
 @EnableEurekaClient
+@RibbonClient(name = "CLOUD-PAYMENT-SERVICE",configuration = MySelfRule.class)
 public class OrderMain80  {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain80.class,args);
