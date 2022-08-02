b50bd24d0dee18e24e28e154e5db014b5a643fcc
Shy <2322621659@qq.com>
2022-08-02 10:40:52 +0800

    使用consul作为注册中心

---
+++ cloud-consumerconsul-order80/src/main/java/com/shy/springcloud/OrderConsulMain80.java
@@ -0,0 +1,17 @@
+package com.shy.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author shy
+ * @version 1.0
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class OrderConsulMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderConsulMain80.class,args);
+    }
+}
---
+++ cloud-consumerconsul-order80/src/main/java/com/shy/springcloud/config/ApplicationContextConfig.java
@@ -0,0 +1,20 @@
+package com.shy.springcloud.config;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author shy
+ * @version 1.0
+ */
+@Configuration
+public class ApplicationContextConfig {
+    @Bean
+    @LoadBalanced
+    public RestTemplate template()
+    {
+        return new RestTemplate();
+    }
+}
