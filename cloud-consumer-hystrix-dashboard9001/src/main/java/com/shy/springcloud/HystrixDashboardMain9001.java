dd42b0f4ae05c24d7fa5bbe2bd66b930b2aadb8f
Shy <2322621659@qq.com>
2022-08-04 15:12:30 +0800

    hystrix实现视图监控

---
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/com/shy/springcloud/HystrixDashboardMain9001.java
@@ -0,0 +1,20 @@
+package com.shy.springcloud;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @author shy
+ * @version 1.0
+ */
+@SpringBootApplication
+@Slf4j
+@EnableHystrixDashboard
+public class HystrixDashboardMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardMain9001.class,args);
+    }
+}
