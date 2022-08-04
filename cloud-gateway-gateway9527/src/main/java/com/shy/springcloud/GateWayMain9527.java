08b56f8d0ba91db1d80132004753f2558df0b4d1
Shy <2322621659@qq.com>
2022-08-04 15:44:14 +0800

    通过配置文件实现gateway

---
+++ cloud-gateway-gateway9527/src/main/java/com/shy/springcloud/GateWayMain9527.java
@@ -0,0 +1,19 @@
+package com.shy.springcloud;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author shy
+ * @version 1.0
+ */
+@SpringBootApplication
+@Slf4j
+@EnableEurekaClient
+public class GateWayMain9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayMain9527.class,args);
+    }
+}
