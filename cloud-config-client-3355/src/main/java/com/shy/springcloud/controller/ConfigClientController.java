e363ec94c9b25e812b7b91e655fabe58b84915f6
Shy <2322621659@qq.com>
2022-08-04 17:54:17 +0800

    实现spring config动态刷新

--- cloud-config-client-3355/src/main/java/com/shy/springcloud/controller/ConfigClientController.java
+++ cloud-config-client-3355/src/main/java/com/shy/springcloud/controller/ConfigClientController.java
@@ -2,6 +2,7 @@ package com.shy.springcloud.controller;
 
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
  * @version 1.0
  */
 @RestController
+@RefreshScope
 public class ConfigClientController {
     @Value("${config.info}")
     private String configInfo;
