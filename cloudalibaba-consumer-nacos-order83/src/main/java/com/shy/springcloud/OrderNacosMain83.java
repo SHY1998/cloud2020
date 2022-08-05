30363a5d5b7c3f8c4523880dea9bb752ea17305b
Shy <2322621659@qq.com>
2022-08-05 11:57:42 +0800

    nacos实现注册中心

---
+++ cloudalibaba-consumer-nacos-order83/src/main/java/com/shy/springcloud/OrderNacosMain83.java
@@ -0,0 +1,17 @@
+package com.shy.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author shy
+ * @version 1.0
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class OrderNacosMain83 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderNacosMain83.class,args);
+    }
+}
